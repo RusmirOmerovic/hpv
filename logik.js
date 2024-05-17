c39160633968265e5467ad5b6b6d8b0914d7d7e8
Rusmir Omerovic <rusmir.omerovic@web.de>
2024-05-17 20:03:10 +0200

    anwendung abgeschlossen bis Fm berechnung

--- logik.js
+++ logik.js
@@ -1,17 +1,58 @@
-let haupt_01 = document.getElementById("haupt1")
-let haupt_02 = document.getElementById("haupt2")
-let haupt_03 = document.getElementById("haupt3")
+var besitzer = document.getElementById("besitzer")
+var ort = document.getElementById("ort")
+var polterNr = document.getElementById("polterNr")
+
+var hoehenListe = []
+
 
 function newDoc() {
-    var x = document.getElementById("wBesitzer").value
-    var y = document.getElementById("ort").value
-    var z = document.getElementById("polterNr").value
-    haupt_01.innerHTML = "Besitzer: " + x
-    haupt_02.innerHTML = "Ort: " + y
-    haupt_03.innerHTML = "Pol-Nr.: " + z
+    var x = document.getElementById("besitzerEingabe").value
+    var y = document.getElementById("ortEingabe").value
+    var z = document.getElementById("polterEingabe").value
+    besitzer.innerHTML = "Besitzer: " + x
+    ort.innerHTML = "Ort: " + y
+    polterNr.innerHTML = "Pol-Nr.: " + z
 }
 
 function newBr() {
     var b = document.getElementById("eingabeBreite").value
-    document.getElementById("massBreite").innerHTML = "Die Polterbreite ist " + b + " Meter"
+    document.getElementById("massBreite").innerHTML = "Polterbreite: " + b + "m"
+}
+
+function zufuegenHoehe() {
+var eingabeHoehe = document.getElementById("hoehen").value
+var gemHoehen = document.getElementById("gemHoehen")
+hoehenListe.push(eingabeHoehe)
+gemHoehen.innerText = "Gemessene Höhen: " + hoehenListe.join(' -- ')
+}
+
+function entfernenHoehe() {
+hoehenListe.pop()
+gemHoehen.innerText = "Gemessene Höhen: " + hoehenListe.join(' -- ')
+}
+
+function berechnen() {
+        var mittenHoehe = document.getElementById("durchschnittHoehe")
+        var sum = hoehenListe.reduce((accumulator, currentValue) => {
+            return Number(accumulator) + Number(currentValue)
+          },0);
+          var finalSum = sum/hoehenListe.length
+       
+       if (hoehenListe.length === 0) {
+          mittenHoehe.innerHTML = `
+        <img src="imgs/durchschnitt.png" alt="durchschnitt" width="20px">
+        ` + " " + "Durchschnittshöhe: " + "Werte leer!"
+    }
+    else {
+        
+        mittenHoehe.innerHTML = `
+        <img src="imgs/durchschnitt.png" alt="durchschnitt" width="20px">
+        ` + " " + "Durchschnittshöhe: " + finalSum + "m" }
+    
+    
+}
+
+
+function ergebnis() {
+    
 }
